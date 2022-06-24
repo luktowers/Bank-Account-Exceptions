95a0d4a87c4c8c0fa8cc47522cca5742548e5ac6
Luktowers <lcs.torss@gmail.com>
2022-06-24 15:32:31 -0300

    Bank account exercise with exceptions

---
+++ src/application/App.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exception.DomainException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder: ");
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+            Account account = new Account(number, holder, balance, withdrawLimit);
+            System.out.print("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+            account.withdraw(amount);
+
+            System.out.println("========");
+            System.out.printf("Account data: %n" + account);
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Invalid entry");
+        }
+        catch (DomainException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+        catch (RuntimeException e){
+            System.out.println("Unexpected error");
+        }
+        sc.close();
+    }
+}
